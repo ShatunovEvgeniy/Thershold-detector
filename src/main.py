4c19681d6865e3ff7c11443996bde4dfb1507084
Shatunov Evgeny <shatunov868@gmail.com>
2025-02-23 15:32:50 +0300

    feat: Add waiting for key for nex frame in main

--- src/main.py
+++ src/main.py
@@ -6,11 +6,12 @@ from processing import draw_bboxes
 from detector import ThresholdDetectController
 
 
-def process_video(video_path: str) -> None:
+def process_video(video_path: str, wait_for_key=None) -> None:
     """
     Opens a video, performs object detection on each frame, draws bounding boxes, and displays the result.
 
     :param video_path: The path to the video file.
+    :param wait_for_key: Wait for specific key for next frame.
     :return: None.
     """
     cap = cv2.VideoCapture(video_path)
@@ -31,14 +32,21 @@ def process_video(video_path: str) -> None:
 
         detections = detector.predict(frame_dto)
 
-        image_with_bboxes = draw_bboxes(detections, frame_dto)
+        image_with_bboxes = draw_bboxes(detections, frame_dto, color=0)
         resized = cv2.resize(
             image_with_bboxes, (1280, 720), interpolation=cv2.INTER_AREA
         )
         cv2.imshow("Video with Detections", resized)
 
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+        if wait_for_key:  # Wait for a specific key press
+            while True:
+                key = cv2.waitKey(0) & 0xFF  # Wait indefinitely for a key press
+                if chr(key) == wait_for_key:
+                    break
+        else:
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        frame_id += 1
 
         frame_id += 1
 
@@ -48,4 +56,4 @@ def process_video(video_path: str) -> None:
 
 if __name__ == "__main__":
     video_path = "video/synthesized.mp4"
-    process_video(video_path)
+    process_video(video_path, wait_for_key=" ")
